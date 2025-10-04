ae2ace242f172a576c73f1e83545da9ec6db1ad1
notBjornMac <notbhalu@gmail.com>
2025-10-03 22:04:16 -0700

    initial commit

---
+++ Scanner.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ali Kooshesh on 9/27/25.
+//
+
+#include "Scanner.hpp"
+
+#include <utility>
+#include <iostream>
+#include <fstream>
+
+#include "utils.hpp"
+
+Scanner::Scanner(std::filesystem::path inputPath) {
+    // You complete this...
+}
+error_type Scanner::tokenize(std::vector<std::string>& words) {
+
+    return NO_ERROR;
+}
